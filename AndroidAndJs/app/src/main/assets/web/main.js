253b9c1c22a87014ab22586a17c270ca8b6e3bbf
fangxiaopeng <850899969@qq.com>
2018-03-06 19:00:38 +0800

    完善业务逻辑

--- AndroidAndJs/app/src/main/assets/web/main.js
+++ AndroidAndJs/app/src/main/assets/web/main.js
@@ -1,21 +1,56 @@
 
 var main = function(){
 
+    var androidInput,showToastBtn,TakePicBtn;
+
     var init = () =>{
         console.log("init");
+
+        initDOM();
+
+        initViews();
 
         bindEvent("on");
     }
 
+    var initDOM = () =>{
+        console.log("initDOM");
+        androidInput = document.getElementById("android_input_txt");
+        showToastBtn = document.getElementById("show_toast");
+        TakePicBtn = document.getElementById("take_picture");
+    }
+
+    var initViews = () => {
+        console.log("initViews");
+
+    }
+
     var bindEvent = method =>{
-        if(method == "on" || method == "off"){
-            console.log("bindEvent-" + method);
+        console.log("bindEvent-" + method);
+        if(method == "on"){
+            showToastBtn.addEventListener("click", showToast, false);
+            TakePicBtn.addEventListener("click", TakePicture, false);
+        }else if(method == "off"){
+            showToastBtn.removeEventListener("click", showToast, false);
+            TakePicBtn.removeEventListener("click", TakePicture, false);
+        }else{
+
         }
     }
 
     var inputAndroidDataToWebView = (str) =>{
-        document.getElementById("android_input_txt").value = str;
-        alert(str);
+        console.log("inputAndroidDataToWebView-" + str);
+        androidInput.value = str;
+    }
+
+    var showToast = () =>{
+        console.log("showToast");
+
+    }
+
+    var TakePicture = () =>{
+        console.log("TakePicture");
+
     }
 
     return{
